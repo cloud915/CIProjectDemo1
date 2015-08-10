c21b22e144fff9ffcf21d2f95a25d6b1de7ea277
cloud915 <cloud_915@sina.com>
2015-08-10 14:10:07 +0800

    Create

---
+++ CIProjectDemo1.UnitTests/MainTest.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using NUnit.Framework;
+
+namespace CIProjectDemo1.UnitTests
+{
+   public class MainTest
+    {
+       public Main main=null;
+
+       [SetUp]
+       public void Setup()
+       {
+           main=new Main();
+       }
+
+       [Test]
+       public void NumberTest()
+       {
+           var result = main.PrintNumberSquare(10);
+
+           Assert.IsInstanceOf<int>(result);
+           Assert.AreEqual(100,result);
+       }
+       [Test]
+       public void StringTest()
+       {
+           var result = main.PrintString("单元测试中");
+
+           Assert.IsInstanceOf<string>(result);
+           Assert.AreEqual("现在输出的内容是：单元测试中", result);
+       }
+
+       [Test]
+       public void StringTest2()
+       {
+           var result = main.PrintString2("单元测试中");
+
+           Assert.IsInstanceOf<string>(result);
+           Assert.AreEqual("修改项目内容，现在输出的内容是：单元测试中", result);
+       }
+
+    }
+}
---
+++ CIProjectDemo1/Main.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CIProjectDemo1
+{
+    public class Main
+    {
+        public int PrintNumberSquare(int num)
+        {
+            return num*num;
+        }
+
+        public string PrintString(string str)
+        {
+            return string.Format("现在输出的内容是：{0}", str);
+        }
+        public string PrintString2(string str)
+        {
+            return string.Format("修改项目内容，现在输出的内容是：{0}", str);
+        }
+
+        public string PrintException(string str)
+        {
+            throw new Exception("异常举例");
+        }
+    }
+}
